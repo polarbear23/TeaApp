384a467843b262b8511f30c60f5cc1fd6b8b3ef2
polarbear23 <xkyd96@gmail.com>
2022-04-02 13:39:00 +0100

    add index.js for server, devstart to package.json

---
+++ src/server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport');
+
+const { SERVER_STATUS } = require('./config.js');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+
+//This will create a middleware.
+//When you navigate to the root page, it will use the built react-app
+if (process.env.NODE_ENV !== 'development') {
+    app.use(express.static(path.resolve(__dirname, '../../build')));
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
+
+app.get('*', (req, res) => {
+    res.json({ ok: true });
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`\n ${SERVER_STATUS.STARTED} ${port}\n`);
+});
