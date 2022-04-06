b0bcbc96e1465b873d53eca033d9e984c36ac5d9
polarbear23 <xkyd96@gmail.com>
2022-04-06 11:09:11 +0100

    add tea interface for props, use data passed down instead of hardcode

--- src/client/pages/homepage/components/TeasGridItem.tsx
+++ src/client/pages/homepage/components/TeasGridItem.tsx
@@ -1,23 +1,41 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const TeasGridItem = () => {
-  const [showInfo, setShowInfo] = useState(false);
-
-  const handleClick = () => {
-    setShowInfo(!showInfo);
+interface TeaProps {
+  tea: {
+    imgFrontUrl: string;
+    isOutOfStock: boolean;
+    name: string;
+    description: string;
+    type: string;
+    amountInStock: number;
+    origin: string;
+    brewTime: number;
+    temperature: number;
   };
+}
 
+const TeasGridItem = (props: TeaProps) => {
+  const { tea } = props;
   return (
     <div className="tea-tile">
       <div className="tea-tile-inner">
-        <div className="tea-tile-img-container" style={{ backgroundImage: '/public/images/teas/' }}></div>
+        <div className="tea-tile-img-container" style={{ backgroundImage: `url(${tea.imgFrontUrl})` }}>
+          <div className="tea-tile-name">
+            <span>{tea.name}</span>
+          </div>
+        </div>
         <div className="tea-tile-info">
-          <span>name</span>
-          <span>type</span>
-          <span>origin</span>
-          <span>brewtime</span>
-          <span>temp</span>
-          <span>desc</span>
+          <div className="tea-info">
+            <span className="tea-tile-name">{tea.name}</span>
+            <span>{tea.description}</span>
+            <span>Type: {tea.type}</span>
+            <span>Origin: {tea.origin}</span>
+            <span>Brew Time: {tea.brewTime} mins</span>
+            <span>Temperature: {tea.temperature}C</span>
+            <Link to="/">
+              <button className="carousel-btn">Buy</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
