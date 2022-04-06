62e90e914f0e0d27509d060f395fde757b5f19be
polarbear23 <xkyd96@gmail.com>
2022-04-06 12:25:26 +0100

    add fetch to get data for tea, add config route for tea

--- src/client/config.ts
+++ src/client/config.ts
@@ -1,19 +1,19 @@
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export const API_URL = {
-    USER_REGISTER: `${SERVER_URL}/user/register`,
-    USER_LOGIN: `${SERVER_URL}/user/login`,
+  USER_REGISTER: `${SERVER_URL}/user/register`,
+  USER_LOGIN: `${SERVER_URL}/user/login`,
+  TEA: `${SERVER_URL}/tea`,
 };
 
 export const HTTP_METHOD = {
-    GET: 'GET',
-    POST: 'POST',
-    PATCH: 'PATCH',
-    PUT: 'PUT',
-    DELETE: 'DELETE',
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
 };
 
 export const LOCAL_STORAGE = {
-    TOKEN: 'token',
+  TOKEN: 'token',
 };
-
