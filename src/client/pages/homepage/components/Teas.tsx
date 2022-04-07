51619b37666c2a0ab16a8d7f68a0692e7c803674
polarbear23 <xkyd96@gmail.com>
2022-04-07 15:31:35 +0100

    add cart quantity props

--- src/client/pages/homepage/components/Teas.tsx
+++ src/client/pages/homepage/components/Teas.tsx
@@ -1,10 +1,18 @@
 import TeasGrid from './TeasGrid';
 import '../../../styles/homepageTeas.css';
-const Teas = () => {
+
+interface StateProps {
+  setCartQuantity: React.Dispatch<React.SetStateAction<number>>;
+  cartQuantity: number;
+}
+
+const Teas = (props: StateProps) => {
+  const { setCartQuantity, cartQuantity } = props;
+
   return (
     <section className="teas">
       <h3 className="section-title">Teas</h3>
-      <TeasGrid />
+      <TeasGrid setCartQuantity={setCartQuantity} cartQuantity={cartQuantity} />
     </section>
   );
 };
