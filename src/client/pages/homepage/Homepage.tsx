9aea9054fc7004aaa8b06d20520e70e62e4f08fc
polarbear23 <xkyd96@gmail.com>
2022-04-07 15:31:22 +0100

    add props and interfaces

--- src/client/pages/homepage/Homepage.tsx
+++ src/client/pages/homepage/Homepage.tsx
@@ -1,13 +1,21 @@
 import Carousel from './components/Carousel';
 import Main from './components/Main';
 import Teas from './components/Teas';
-const Homepage = () => {
+
+interface StateProps {
+  setCartQuantity: React.Dispatch<React.SetStateAction<number>>;
+  cartQuantity: number;
+}
+
+const Homepage = (props: StateProps) => {
+  const { setCartQuantity, cartQuantity } = props;
+
   return (
     <main className="main">
       <div className="homepage">
         <Carousel />
         <Main />
-        <Teas />
+        <Teas setCartQuantity={setCartQuantity} cartQuantity={cartQuantity} />
       </div>
     </main>
   );
