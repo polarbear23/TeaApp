daa1dc64fcbd8e2d1f9f1e9d098377993f31d2a7
polarbear23 <xkyd96@gmail.com>
2022-04-07 15:33:47 +0100

    add get and post route for order

--- src/server/routes/order.js
+++ src/server/routes/order.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const { createOrder, updateOrder } = require('../controllers/order');
+const { createOrder, updateOrder, upsertOrderProducts, getAllOrders } = require('../controllers/order');
 
+
+router.get('/', passport.authenticate('jwt', { session: false }), getAllOrders);
 router.post('/', passport.authenticate('jwt', { session: false }), createOrder);
-
 router.put('/', passport.authenticate('jwt', { session: false }), updateOrder);
+router.post('/product', upsertOrderProducts);//for single order
 
 module.exports = router;
