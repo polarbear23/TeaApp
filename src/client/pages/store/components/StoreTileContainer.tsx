31bf659bcf869eda44c135b9b14060f2865b9f24
polarbear23 <xkyd96@gmail.com>
2022-04-12 12:13:29 +0100

    add teas state to store, add fetch for teas data from db

--- src/client/pages/store/components/StoreTileContainer.tsx
+++ src/client/pages/store/components/StoreTileContainer.tsx
@@ -1,8 +1,22 @@
 import StoreTile from './StoreTile';
+import { useEffect, useState } from 'react';
+import { API_URL } from '../../../config';
 const StoreTileContainer = () => {
+  const [teas, setTeas] = useState([]);
+  useEffect(() => {
+    const fetchTea = async () => {
+      const res = await fetch(API_URL.TEA);
+      const result = await res.json();
+      console.log('tea', result.data);
+      setTeas(result.data);
+    };
+    fetchTea();
+  }, []);
   return (
     <div className="store-tile-container">
-      <StoreTile></StoreTile>
+      {teas.map((tea) => (
+        <StoreTile></StoreTile>
+      ))}
     </div>
   );
 };
