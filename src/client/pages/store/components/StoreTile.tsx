82be486e01f164ebc7c728bc0b5d3f6df73080f9
polarbear23 <xkyd96@gmail.com>
2022-05-17 14:33:35 +0100

    add props to store components for tiles, add handle click to allow cart items to be added

--- src/client/pages/store/components/StoreTile.tsx
+++ src/client/pages/store/components/StoreTile.tsx
@@ -1,11 +1,38 @@
+import { Product } from '../../../App';
 import { Tea } from '../../homepage/components/TeasGrid';
+import { postFormToServer } from '../../../utils/fetchPostForms';
+import { API_URL } from '../../../config';
 
 interface StateProps {
   tea: Tea;
+  setCartQuantity: React.Dispatch<React.SetStateAction<number>>;
+  cartQuantity: number;
+  setCart: React.Dispatch<React.SetStateAction<Product[]>>;
+  cart: Product[];
+  product: Product;
 }
 
 const StoreTile = (props: StateProps) => {
-  const { tea } = props;
+  const { tea, setCart, cart, cartQuantity, setCartQuantity, product } = props;
+
+  const handleClick = async (): Promise<void> => {
+    if (localStorage.getItem('user') !== null) {
+      const user = JSON.parse(localStorage.getItem('user') || '');
+      const result = await postFormToServer(API_URL.ORDERPRODUCTS, { productId: product.id, orderId: user.order[user.order.length - 1].id });
+      const newCart: Product[] = [...cart, product];
+      setCart(newCart);
+      localStorage.setItem('cart', JSON.stringify(newCart));
+      setCartQuantity(cartQuantity + 1);
+      console.log('added to cart', result);
+    }
+    const newCart: Product[] = [...cart, product];
+    setCart(newCart);
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    setCartQuantity(cartQuantity + 1);
+    console.log('added to cart', product);
+    // if there is no user then do nothing
+  };
+
   return (
     <div className="store-item-tile">
       <div className="store-item-img-section" style={{ backgroundImage: `url(${tea.imgUrl})` }}>
@@ -16,7 +43,7 @@ const StoreTile = (props: StateProps) => {
         <span>Type: {tea.type}</span>
         <span>Origin: {tea.origin}</span>
         <span>Price: {tea.product.price / 100}</span>
-        <button>Buy</button>
+        <button onClick={handleClick}>Buy</button>
       </div>
     </div>
   );
